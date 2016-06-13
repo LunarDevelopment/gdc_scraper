02c9c4f6cba98a6a13f648a6d8a397e0f5492585
Priyav Shah <priyavrocks@gmail.com>
2016-06-13 15:14:56 +0100

    threading in progress, integration tests in progress

--- gmaps_geolocate.py
+++ gmaps_geolocate.py
@@ -1,8 +1,8 @@
 from googlemaps import Client
 from googlemaps.exceptions import Timeout
-from pymongo import MongoClient
 import pprint
 from time import sleep
+from pymongo import UpdateOne
 
 APIKEY = "AIzaSyDDj7EvYk-arWEqLFuMZeWcQ7cw6MaPhms"
 
@@ -17,9 +17,11 @@ class GmapsGeocoder():
         """
         self.gmaps = gmaps
         self.members = members
+        self.data_store = data_store
 
     def get_member_location(self, member):
         address = member.get("address", False)
+        print("address: {}".format(address))
         if not address:
             return False
 
@@ -28,23 +30,56 @@ class GmapsGeocoder():
         if len(geocode_result) > 0:
             return geocode_result[0]["geometry"]["location"]
         else:
+            print(geocode_result)
+            print("Not enough results found")
             return False
 
     def get_member_locations(self):
         # while queue is not empty
-        while not self.queue.empty():
-            member = self.queue.get()
+        while not self.members.empty():
+            member = self.members.get()
             geoPoint = self.get_member_location(member)
-
+            print("geoPoint = {}".format(geoPoint))
             if geoPoint:
-                self.data_store.push(geoPoint)
+                self.data_store.append(
+                    UpdateOne(
+                        {"_id": member["_id"]},
+                        {"$set": {"location": geoPoint}},
+                        upsert=True
+                    )
+                )
+
+            self.members.task_done()
 
 
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    from threading import Thread
+    from queue import Queue
 
-    def run():
-        # Get data from that col
-        # 
-        pass
+    c = MongoClient()["tubules"]["members"]
+    no_location_members = c.find({"location": {"$exists": False}})
+
+    # client = Client(key=APIKEY)
+    members = Queue()
+    data_store = []
+
+    g = GmapsGeocoder(members, data_store)
+    threads = [Thread(target=g.get_member_locations) for i in range(10)]
+
+    for i in no_location_members:
+        members.put(i)
+
+    for t in threads:
+        t.start()
+    try:
+        members.join()
+    except Exception:
+        print("In exception")
+    finally:
+        c.bulk_write(data_store)
+
+
 
 
 
--- tests/test_gmaps-geolocate.py
+++ tests/test_gmaps-geolocate.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 # from unittest import Mock
 from gmaps_geolocate import GmapsGeocoder
+from mongomock import Mock
+from queue import Queue
 
 
 # class test_GmapsGeocoder(TestCase):
@@ -21,7 +23,8 @@ class test_gmaps_geolocate(TestCase):
     def test_GmapsGeocoder_get_member_location(self):
         g = GmapsGeocoder(None, None)
 
-        l = g.get_member_location({"address": "27 Mountbel Road, stanmore, UK"})
+        l = g.get_member_location(
+            {"address": "27 Mountbel Road, stanmore, UK"})
         print("l = {}".format(l))
         self.assertEqual({'lng': -0.3232248, 'lat': 51.60104279999999}, l)
 
@@ -43,17 +46,18 @@ class test_gmaps_geolocate(TestCase):
     def test_GmapsGeocoder_get_member_location_isle_of_man(self):
         g = GmapsGeocoder(None, None)
         correctAddress = {"lat": 54.1562134, "lng": -4.4822503}
-        l = g.get_member_location({"address": "47 Derby Square, Douglas, Isle of Man, IM1 3LP, Isle of Man"})
+        l = g.get_member_location(
+            {"address": "47 Derby Square, Douglas, Isle of Man, IM1 3LP, Isle of Man"})
         print("l = {}".format(l))
         self.assertEqual(l, correctAddress)
 
     def test_GmapsGeocoder_get_member_location_whales_town(self):
         g = GmapsGeocoder(None, None)
         correctAddress = {"lat": 51.61578, "lng": -3.415012}
-        l = g.get_member_location({"address": "26 Grawen Street, Porth, Rhondda Cynon Taff, CF39 0BU, United Kingdom"})
+        l = g.get_member_location(
+            {"address": "26 Grawen Street, Porth, Rhondda Cynon Taff, CF39 0BU, United Kingdom"})
         print("l = {}".format(l))
         self.coordinate_checker(l, correctAddress)
-
 
     # def test_GmapsGeocoder_should_not_push_when_no_data_passed_false(self):
     #     q = Queue()
@@ -62,7 +66,30 @@ class test_gmaps_geolocate(TestCase):
     #     g = GmapsGeocoder(q, rq, None)
     #     g.get_member_location = Mock(return_value=False)
 
-
+    def test_GmapsGecoder_intergration_test(self):
+        member_list = [
+            {"Qualifications": "Qual- BSc Honours in Dental Technology Cardiff 2014", "Registrant Type": "Dental Care Professional", "Registration Number": 252911, "Status": "Registered",
+                "address": "14 Royal Parade Mews, Cheltenham, Gloucestershire, GL50 1SZ, United Kingdom", "firstname": "Sameera ", "location": {"lat": 51.8963537, "lng": -2.0822569}, "surname": "Patel", "tubuleMember": True},
+            {"Qualifications": "Dip Orth Therapy RCPS Eng 2010", "Registrant Type": "Dental Care Professional", "Registration Number": 110107, "Status": "Registered",
+                "address": "Lakeside Specialist Orthodontics Practice, 38 Salisbury Ave, Wirral, Merseyside, CH48 0QP, United Kingdom", "firstname": "Lucy ", "location": {"lat": 53.37120849999999, "lng": -3.1873406}, "surname": "Evans", "tubuleMember": True},
+            {"Qualifications": "Diploma in Dental Nursing Level 3 QCF City & Guilds 2015", "Registrant Type": "Dental Care Professional", "Registration Number": 259592, "Status": "Registered",
+                "address": "31 Lingen Close, Shrewsbury, Shropshire, SY1 2UN, United Kingdom", "firstname": "Lucy Annabelle", "location": {"lat": 52.7204636, "lng": -2.738369}, "surname": "Evans", "tubuleMember": True},
+            {"Qualifications": "BDS Sheff 2009", "Registrant Type": "Dentist", "Registration Number": 177819, "Status": "Registered",
+                "address": "Hockley Dental Surgery, 2 Woodlands Parade, Main Road, Hockley, Essex, SS5 4QU, UNITED KINGDOM", "firstname": "Teki ", "location": {"lat": 51.6011237, "lng": 0.6535264}, "surname": "Sowdani", "tubuleMember": True},
+            {"Qualifications": "NVQ L3 Oral Health Care:Dental Nursing& Indp Asses City & Guilds 2008", "Registrant Type": "Dental Care Professional", "Registration Number": 160793, "Status": "Registered",
+                "address": "195 Thunder Lane, NORWICH, Norfolk, NR7 0JF, United Kingdom", "firstname": "Gemma Claire", "location": {"lat": 52.63631609999999, "lng": 1.3430194}, "surname": "Spencer", "tubuleMember": True},
+            {"Qualifications": "NVQ L3 Oral Health Care:Dental Nursing& Indp Asses City & Guilds 2007", "Registrant Type": "Dental Care Professional", "Registration Number": 141437, "Status": "Registered",
+                "address": "8 Sandstone Road, Milnrow, Rochdale, Greater Manchester, OL16 3UH, United Kingdom", "firstname": "Gemma Wendy", "location": {"lat": 53.614079, "lng": -2.1128219}, "surname": "Spencer", "tubuleMember": True},
+            {"Qualifications": "BDS Ncle 1995", "Registrant Type": "Dentist", "Registration Number": 70566, "Status": "Registered", "address": "Dental Surgery, 78 Kenton Lane, Newcastle upon Tyne, Tyne and Wear, NE3 4LE, United Kingdom",
+                "firstname": "Annabelle Jane", "location": {"lat": 55.00312109999999, "lng": -1.63958}, "surname": "Ford", "tubuleMember": True},
+            {"Qualifications": "Qual- National Certificate NEBDN 2010", "Registrant Type": "Dental Care Professional", "Registration Number": 213984, "Status": "Registered",
+                "address": "Glendale Manor, Collaroy Road, Cold Ash, Thatcham, Berkshire, RG18 9PB, UNITED KINGDOM", "firstname": "Rebecca Claire", "location": {"lat": 51.4214753, "lng": -1.2631128}, "surname": "Holmes", "tubuleMember": True},
+            {"Qualifications": "Diploma in Dental Nursing Level 3 QCF City & Guilds 2015", "Registrant Type": "Dental Care Professional", "Registration Number": 255870, "Status": "Registered",
+                "address": "Dental Surgery, 30 Highgate Lane, Lepton, Huddersfield, West Yorkshire, HD8 0HB, United Kingdom", "firstname": "Rebecca Louise", "location": {"lat": 53.6333496, "lng": -1.7121814}, "surname": "Holmes", "tubuleMember": True}
+        ]
+        data_store = []
+        members = Queue()
+        g = GmapsGeocoder(members, data_store)
 
 if __name__ == "__main__":
     unittest.main()
